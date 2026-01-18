49f67e7822c18c534cd46c6c6520381c15433c78
Yoshinobu Date <82716291+yoshi3jp@users.noreply.github.com>
2026-01-18 22:59:21 +0900

    fix keymaster formatting
    Change-Id: I9fa67303c0a7cd356ef05cfd3312987f025a2849

--- device.mk
+++ device.mk
@@ -18,4 +18,11 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service
 
 PRODUCT_VENDOR_PROPERTIES += \
+<<<<<<< HEAD
     ro.vendor.keymaster.version=3.0
+=======
+    ro.vendor.keymaster.version=3.0 \
+ 	ro.hardware.keystore_desede=true \
+ 	ro.hardware.keystore=software \
+ 	ro.hardware.gatekeeper=software
+>>>>>>> 1a7d354 (Fix formatting in device.mk for keymaster version)
